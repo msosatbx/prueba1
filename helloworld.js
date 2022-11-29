e64820d18189722406ae94a9455dea135225f495
msosatbx <msosa@toolboxtve.com>
2022-11-29 09:42:59 -0300

    algo

--- helloworld.js
+++ helloworld.js
@@ -11,9 +11,6 @@ const algomas2 = 'algomas'
 const algomas3 = 'algomas'
 const algomas4 = 'algomas'
 const algomas5 = 'algomas'
-<<<<<<< HEAD
 const algomas6 = 'algomas'
-=======
 
 let intente = 'escribir algo'
->>>>>>> 6cf3f0d7beff05f94384234a8c2ae4bf6e9cadac
